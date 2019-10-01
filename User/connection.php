ec2cf48ccc53dcaa78ab4b2cb5265e67ee731ba6
NazirMd <51619256+NazirMd@users.noreply.github.com>
2019-10-01 18:25:53 +0530

    Update connection.php

--- User/connection.php
+++ User/connection.php
@@ -1,12 +1,16 @@
 <?php
 
 
-$servername="localhost";
-$username="mansurbaban";
-$password="Khadarsil2018";
-//$dbname="mansurba_wantreformdb";
-$dbname="mansurba_customerdb";
+//$servername="localhost";
+//$username="mansurbaban";
+//$password="Khadarsil2018";
+//$dbname="mansurba_customerdb";
 
+$servername="z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username="xvi1krqqd2x3g1oo";
+$password="k5cv5z51owpxy27p";
+$dbname="y0827d64hc2cp22e";
+ 
 $conn=  mysqli_connect($servername, $username, $password, $dbname);
 
 if(!$conn)
