4d0a78d1c50e4caf21ad0c10b72df76110351982
NazirMd <51619256+NazirMd@users.noreply.github.com>
2020-05-04 15:12:54 +0530

    Update Get_Staff_Details.php

--- User/Get_Staff_Details.php
+++ User/Get_Staff_Details.php
@@ -1,1 +1,18 @@
-echo "HI";
+<?php
+
+require_once('connection.php');   
+	
+$query = "SELECT Staff_Name, Staff_Designation,Staff_Mobile_Number,Staff_Login_Name,Mandal_Name,District_Name,Village_Ward_Name from staff_info";
+
+
+$result=  mysqli_query($conn, $query);
+
+while($row  = mysqli_fetch_assoc($result)){
+    
+    $data[]=$row;
+}
+
+    echo json_encode($data); 
+
+ mysqli_close($conn);
+?>
